62744f8c13a8c60796498c25631b4fb6f589da9e
seano424 <soreilly424@gmail.com>
2024-01-25 17:37:56 -0600

    creating ig links

--- components/InstagramGrid.tsx
+++ components/InstagramGrid.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import Image from 'next/image'
 
 type InstagramPostsData = {
@@ -34,19 +35,49 @@ export default async function InstagramGrid() {
       </h4>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
         {data &&
-          data.data.slice(0, 8).map(({id, thumbnail_url, media_url}) => (
-            <div
-            key={id}
-              className="bg-blue-100 relative h-[400px]"
-            >
-              <Image
-                fill
-                alt="Sean O'Reilly"
-                src={thumbnail_url ? thumbnail_url : media_url}
-                className="object-cover object-center w-full h-full"
-              />
-            </div>
-          ))}
+          data.data
+            .slice(0, 8)
+            .map(({id, thumbnail_url, media_url, permalink}) => (
+              <Link
+                target="_blank"
+                href={permalink}
+                key={id}
+                className="bg-blue-100 relative h-[300px]"
+              >
+                <Image
+                  fill
+                  alt="Sean O'Reilly"
+                  src={thumbnail_url ? thumbnail_url : media_url}
+                  className="object-cover object-center w-full h-full"
+                />
+                {thumbnail_url && (
+                  <div className="absolute inset-0 flex items-center justify-center text-white">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="40"
+                      height="40"
+                      viewBox="0 0 24 24"
+                      strokeWidth="2"
+                      stroke="currentColor"
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path
+                        stroke="none"
+                        d="M0 0h24v24H0z"
+                        fill="none"
+                      />
+                      <path
+                        d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z"
+                        strokeWidth="0"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </div>
+                )}
+              </Link>
+            ))}
       </div>
     </section>
   )
