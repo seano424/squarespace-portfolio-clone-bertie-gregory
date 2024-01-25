b6d25b423b460db2225b3d5d380ff92a963e20f2
seano424 <soreilly424@gmail.com>
2024-01-25 15:08:28 -0600

    adding pathname to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,8 @@
+'use client'
+
+import clsx from 'clsx'
 import Link from 'next/link'
+import {usePathname} from 'next/navigation'
 
 const navLinks = [
   {
@@ -6,24 +10,26 @@ const navLinks = [
     label: 'home',
   },
   {
-    href: '/',
+    href: '/projects',
     label: 'web projects',
   },
   {
-    href: '/',
+    href: '/stills',
     label: 'stills',
   },
   {
     href: '/about',
-    label: 'contact',
+    label: 'about',
   },
   {
-    href: '/latest',
+    href: '/contact',
     label: 'contact',
   },
 ]
 
 export default function Header() {
+  const pathname = usePathname()
+
   return (
     <header className="text-center pt-7 pb-12 uppercase tracking-widest">
       <Link
@@ -33,16 +39,21 @@ export default function Header() {
         Sean O'Reilly
       </Link>
 
-      <ul className="mb-8 mt-5 flex text-[13px] gap-4 text-stone-200 justify-center">
+      <ul className="mb-8 mt-4 flex text-[12px] gap-4 text-stone-200 justify-center">
         <li>Web Developer</li>
         <li>Photographer</li>
         <li>World Traveler</li>
       </ul>
 
-      <nav className="flex justify-between items-center text-stone-500 text-[13px]">
+      <nav className="flex justify-between items-center text-stone-500 text-[12px]">
         <span className="w-2 h-[1px] bg-stone-500 rounded" />
         {navLinks.map(({href, label}) => (
           <Link
+            className={clsx(
+              'hover:text-stone-800 pb-[2px] hover:border-b-2 hover:border-stone-300',
+              href === pathname && 'text-stone-800 border-b-2 border-stone-300',
+              'transition-all duration-200 ease-in-out'
+            )}
             key={href}
             href={href}
           >
